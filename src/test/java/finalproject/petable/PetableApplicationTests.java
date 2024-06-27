81918ef697a523a2f312b673a4df8342eeed663c
Kiril Nikolov <kirqkov2309@gmail.com>
2024-06-27 14:34:07 +0300

    First commit. Base templates and app structure.

---
+++ src/test/java/finalproject/petable/PetableApplicationTests.java
@@ -0,0 +1,13 @@
+package finalproject.petable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
