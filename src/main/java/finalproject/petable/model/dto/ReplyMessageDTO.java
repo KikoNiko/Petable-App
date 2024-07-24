4970bf68fa58faa320b558f9e925cafd8e87a690
Kiril Nikolov <kirqkov2309@gmail.com>
2024-07-24 12:44:53 +0300

    message sending

---
+++ src/main/java/finalproject/petable/model/dto/ReplyMessageDTO.java
@@ -0,0 +1,14 @@
+package finalproject.petable.model.dto;
+
+public class ReplyMessageDTO {
+
+    private String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
