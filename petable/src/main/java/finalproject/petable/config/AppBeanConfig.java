29bc21e2e8d8367a60f754bed27934ab3c2e5db4
Kiril Nikolov <kirqkov2309@gmail.com>
2024-08-07 11:49:10 +0300

    i18n internationalization

--- petable/src/main/java/finalproject/petable/config/AppBeanConfig.java
+++ petable/src/main/java/finalproject/petable/config/AppBeanConfig.java
@@ -1,12 +1,13 @@
 package finalproject.petable.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
 public class AppBeanConfig {
-
     @Bean
     ModelMapper modelMapper() {
         return new ModelMapper();
